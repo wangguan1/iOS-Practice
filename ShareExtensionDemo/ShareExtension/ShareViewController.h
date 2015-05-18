0e35d0dceaa97d37f31dc9a801258b6341f91d57
Guan Wang <wangguan@iyunxiao.com>
2015-05-18 15:06:34 +0800

    Template project.

---
+++ ShareExtensionDemo/ShareExtension/ShareViewController.h
@@ -0,0 +1,14 @@
+//
+//  ShareViewController.h
+//  ShareExtension
+//
+//  Created by MacPro on 5/18/15.
+//  Copyright (c) 2015 MacPro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
---
+++ ShareExtensionDemo/ShareExtensionDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShareExtensionDemo
+//
+//  Created by MacPro on 5/18/15.
+//  Copyright (c) 2015 MacPro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
